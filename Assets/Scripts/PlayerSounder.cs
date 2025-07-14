7cd4849805a1aac275c1ecc1fe4b30004138c433
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-14 11:21:13 +0900

    add sound

---
+++ Assets/Scripts/PlayerSounder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerSounder : MonoBehaviour
+{
+    [SerializeField] private AudioClip _audioClip;
+    [SerializeField] [Range(0, 1)] private float _volume;
+
+    private AudioSource _audioSource;
+    private Rigidbody2D _rb;
+    private bool _isGround;
+    private bool _isDown;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if ( _audioSource == null)
+            _audioSource = gameObject.AddComponent<AudioSource>();
+
+        _rb.GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        bool isDown = _rb.linearVelocityY < 0;
+
+        if (_isDown != isDown)
+        {
+            _isDown = isDown;
+            if (_isDown == false)
+            {
+                _audioSource.PlayOneShot(_audioClip, _volume);
+            }
+        }
+    }
+}
